a7bd8c776524e43c8777e799765eb90f1e3b8c32
danielagee <danielhouston15@yahoo.com>
2022-11-09 07:13:22 +0200

    Add Moving Averages 1. Integrate reset.index with ticker.history 2. Import 5 years of data with period=’5y’ 3. Calculate the 21 and 200 day moving average 4. Add a trace for each moving average to the plot with fig.add_trace

--- run.py
+++ run.py
@@ -1,22 +1,23 @@
-# Day 50 - Build a candlestick plot in plotly
-# Install plotly package
-# Documentation can be found here:
-# https://plotly.com/python/candlestick-charts/
+# Day 51 - Add 21 and 200 day moving averages.
 
 import yfinance as yf
-# Import plotly to plot the data as graph objects (go).
 import plotly.graph_objects as go
 
 stock = 'GOOGL'  # Input the ticker for the stock you want to view.
 ticker = yf.Ticker(stock)  # Gets the correct stock ticker data.
-df = ticker.history()  # Downloads historical data for open, high, low, close, volume, dividends, splits to a dataframe.
+# Download 5 years of data and use actual rather than adjusted prices
+df = ticker.history(period='5y', auto_adjust=False).reset_index()  # reset_index() to add proper index column
 
-# The original format uses the date column as the index. As we need an index column for plotly,
-# Before we plot, we need to re-index the data set and store the original index (date) as a new column.
-df = df.reset_index()
+# Define the 21 and 200 day moving average calculations from closing price
+df['21dma'] = df['Close'].rolling(window=21).mean()
+df['200dma'] = df['Close'].rolling(window=200).mean()
 
 # Basic candlestick plot using date, open, high, low, and close.
 fig = go.Figure(data=[go.Candlestick(x=df['Date'], open=df['Open'], high=df['High'],
                                        low=df['Low'], close=df['Close'])])
+
+# Plot the 21 and 200 day moving averages as new traces on the plotly chart.
+fig.add_trace(go.Scatter(x=df['Date'], y=df['21dma'], line=dict(color='#000000'), name="21-Day-MA"))
+fig.add_trace(go.Scatter(x=df['Date'], y=df['200dma'], line=dict(color='#000000'), name="200-Day-MA"))
 
 fig.show()
